0c8af8b532bc5396fab8cedc3a7ec4be6bac50b6
Alexander Liu <git@alexanderliu.com>
2024-04-21 03:40:32 -0700

    remove turbo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
-// This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  root: true,
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
